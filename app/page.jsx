be0dbbb08d786d03321ca880bdee560e9113c61f
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-02-24 23:59:55 +0530

    Working User Profile Added

--- app/page.jsx
+++ app/page.jsx
@@ -12,6 +12,9 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 
+import { howItWorks } from "@/data/howItWorks";
+import { testimonial } from "@/data/testimonial";
+
 
 export default function Home() {
   return <div>
@@ -63,6 +66,56 @@ export default function Home() {
             <h3 className="text-4xl font-bold">24/7</h3>
             <p className="text-muted-foreground">AI Support</p>
           </div>
+        </div>
+      </div>
+    </section>
+
+    <section className="w-full py-12 md:py-24 lg:py-32 bg-background">
+      <div className="container mx-auto px-4 md:px-6">
+      <div className="text-center max-w-3xl mx-auto mb-12">
+        <h2 className="text-3xl font-bold mb-4"> How It Works</h2>
+        <p>
+          Four simple steps to accelerates your carrer growth
+        </p>
+      </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 max-w-6xl mx-auto">
+          {howItWorks.map((item, index) => {
+            return (
+              <div 
+              key={index}
+              className="flex flex-col items-center text-center space-y-4"
+              >
+                <div className="w-16 h-16 rounded-full bg-primary/10 flex items-center justify-center">
+                {item.icon}
+                </div>
+                <h3 className="font-semibold text-xl">{item.title}</h3>
+                <p className="text-muted-foreground">{item.description}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+
+    <section className="w-full py-12 md:py-24 lg:py-32 bg-background">
+      <div className="container mx-auto px-4 md:px-6">
+        <h2 className="text-3xl font-bold tracking-tighter text-center mb-12"> What Our Users Say</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 max-w-6xl mx-auto">
+          {testimonial.map((testimonial, index) => {
+            return (
+              <Card 
+              key={index}
+              className = "bg-background"
+              >
+                <CardContent className="pt-6">
+                  <div>
+                    <div></div>
+                    <blockquote></blockquote>
+                  </div>
+                </CardContent>
+              </Card>
+            );
+          })}
         </div>
       </div>
     </section>
