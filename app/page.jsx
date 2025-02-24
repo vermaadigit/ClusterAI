44a3fff9d589642d2e48a540f6c8abab354e49e6
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-02-24 17:57:55 +0530

    Extension changed to .js --> .jsx

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import HeroSection from "@/components/hero";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return <div>
+
+    <div className="grid-background"></div>
+
+    <HeroSection />
+
+  </div>
+}
