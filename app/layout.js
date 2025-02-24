31229791b98544ca0d9394757ddfb729b0cdfcbc
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-02-24 15:50:33 +0530

    Dark Theme

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      baseTheme: dark,
+    }}>
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className}`}
