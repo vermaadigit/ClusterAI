ba3a792834da06ff5da16ab1e59093f2005cff03
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-02-26 02:10:02 +0530

    Landing Page Compleate

--- app/page.jsx
+++ app/page.jsx
@@ -21,6 +21,8 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion"
+import Link from "next/link";
+import { ArrowRight } from "lucide-react";
 
 
 export default function Home() {
@@ -181,6 +183,30 @@ export default function Home() {
           })}
         </Accordion>
         </div>
+      </div>
+    </section>
+
+    <section className="w-full">
+      <div className="mx-auto py-24 gradient rounded-lg">
+      <div className="flex flex-col items-center justify-center space-y-4 text-center max-w-3xl mx-auto">
+        <h2 className="text-3xl font-bold tracking-tighter text-primary-foreground sm:text-4xl md:text-5xl">
+          Ready to Accelerate Your Career?
+        </h2>
+        <p className="mx-auto max-w-[600px] text-primary-foreground/80 md:text-xl">
+          Join thousands of professionals who are advancing their carrers
+          with AI-powered guidance.
+        </p>
+        <Link href="/dashboard" passHref>
+          <Button
+            size="lg"
+            variant = "secondary"
+            className = "h-11 mt-5 animate-bounce"
+          >
+            Start Your Journey Today <ArrowRight className="ml-2 h-4 w-4" />
+          </Button>
+        </Link>
+      </div>
+        
       </div>
     </section>
 
