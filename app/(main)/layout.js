c38e22bf36c6e40f339b8e12e3705240456e3b99
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-03-06 19:29:28 +0530

    Layout Page

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const MainLayout = ({children}) => {
+
+    //Redirecting to onboarding 
+
   return (
     <div className='container mx-auto mt-24 mb-20'>{children}</div>
   )
