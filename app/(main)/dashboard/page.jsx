f0ed62c8b37945d286fe4949ee52e9c1ba883dbc
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-03-09 20:00:14 +0530

    Dashboard Page

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,5 +1,14 @@
+import { getUserOnboardingStatus } from "@/actions/user";
+import { redirect } from "next/navigation";
 
-const IndustryInsightsPage = () => {
+const IndustryInsightsPage = async () => {
+
+      const {isOnboarded} = await getUserOnboardingStatus();
+  
+      if (!isOnboarded) {
+        redirect("/onboarding");
+      }
+
   return (
     <div>IndustryInsightsPage</div>
   )
