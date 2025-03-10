7afa34f2c00f9f8bb92924d205966f6683bfb148
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-03-10 18:01:57 +0530

    inngest routes api

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,10 +1,9 @@
-import { inngest } from "@/lib/inngest/client";
 import { serve } from "inngest/next";
 
-// Create an API that serves zero functions
+import { inngest } from "@/lib/inngest/client";
+import { generateIndustryInsights } from "@/lib/inngest/function";
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [
-    
-  ],
+  functions: [generateIndustryInsights],
 });
