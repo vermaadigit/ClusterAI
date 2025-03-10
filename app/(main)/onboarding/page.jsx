081b34db3f1859acdc961931293c54cffdc317ed
vermaadigit <adityaverma_2021@vitbhopal.ac.in>
2025-03-10 17:54:02 +0530

    onboarding page

--- app/(main)/onboarding/page.jsx
+++ app/(main)/onboarding/page.jsx
@@ -1,22 +1,19 @@
-import { getUserOnboardingStatus } from "@/actions/user"
-import { industries } from "@/data/industries"
 import { redirect } from "next/navigation";
+import { industries } from "@/data/industries";
 import OnboardingForm from "./_components/onboarding-form";
+import { getUserOnboardingStatus } from "@/actions/user";
 
-const OnboardingPage = async() => {
+export default async function OnboardingPage() {
+  // Check if user is already onboarded
+  const { isOnboarded } = await getUserOnboardingStatus();
 
-    //Check if user is already onboarding
-    const {isOnboarded} = await getUserOnboardingStatus();
-
-    if (isOnboarded) {
-      redirect("/dashboard");
-    }
+  if (isOnboarded) {
+    redirect("/dashboard");
+  }
 
   return (
     <main>
-        <OnboardingForm industries={industries} />
+      <OnboardingForm industries={industries} />
     </main>
-  )
+  );
 }
-
-export default OnboardingPage
